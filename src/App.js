00a1bd055485e7019673a37f62f38ec2bdfef557
washben99 <washben99@gmail.com>
2024-04-15 20:19:01 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Mensaje from './Mensaje';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje color='green' message='Estamos trabajando'/>
+      <Mensaje color='blue' message='En un curso'/>
+      <Mensaje color='yellow' message='de react'/>
     </div>
   );
 }
